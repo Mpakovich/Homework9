c3a55de33becbb7e67a086838dd8369e01e4db77
Mpakovich <56260995f@gmail.com>
2025-01-23 18:39:17 +0200

    some small fixes

--- tests/PostOrder.spec.ts
+++ tests/PostOrder.spec.ts
@@ -3,127 +3,125 @@ import { expect, test } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
 
 // Home work9
-
-
 test('Post order with unsupported status value should receive code 400', async ({ request }) => {
-    const requestBody = {
-        status: 'INVALID_STATUS', // Unsupported value
-        courierId: 0,
-        customerName: 'John Doe',
-        customerPhone: '+1234567890',
-        comment: 'Urgent delivery',
-        id: 1,
-    };
+  const requestBody = {
+    status: 'INVALID_STATUS', // Unsupported value
+    courierId: 0,
+    customerName: 'John Doe',
+    customerPhone: '+1234567890',
+    comment: 'Urgent delivery',
+    id: 1,
+  }
 
-    const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-        data: requestBody,
-    });
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: requestBody,
+  })
 
-    const responseBody = await response.text();
+  const responseBody = await response.text()
 
-    console.log('response status:', response.status());
-    console.log('response body:', responseBody);
+  console.log('response status:', response.status())
+  console.log('response body:', responseBody)
 
-    expect(response.status()).toBe(StatusCodes.BAD_REQUEST);
-});
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
 
 //Homework 9
 test('Post order with empty data should receive code 400', async ({ request }) => {
-    const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-        data: {}, // Empty data
-    });
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: {}, // Empty data
+  })
 
-    const responseBody = await response.json();
+  const responseBody = await response.json()
 
-    console.log('response status:', response.status());
-    console.log('response body:', responseBody);
+  console.log('response status:', response.status())
+  console.log('response body:', responseBody)
 
-    expect(response.status()).toBe(StatusCodes.BAD_REQUEST); //expect result status 400, Received status 200 test failed
-});
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST) //expect result status 400, Received status 200 test failed
+})
 
 //Homework9
 test('Post order with invalid customerPhone should receive code 400', async ({ request }) => {
-    const requestBody = {
-        status: 'OPEN',
-        courierId: 0,
-        customerName: 'John Doe',
-        customerPhone: 12, // wrong phone number
-        comment: 'Urgent delivery',
-        id: 1,
-    };
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'John Doe',
+    customerPhone: 12, // wrong phone number
+    comment: 'Urgent delivery',
+    id: 1,
+  }
 
-    const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-        data: requestBody,
-    });
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: requestBody,
+  })
 
-    const responseBody = await response.json();
+  const responseBody = await response.json()
 
-    console.log('response status:', response.status());
-    console.log('response body:', responseBody);
+  console.log('response status:', response.status())
+  console.log('response body:', responseBody)
 
-    expect(response.status()).toBe(StatusCodes.BAD_REQUEST); //expect result status 400, Received status 200 test failed
-});
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST) //expect result status 400, Received status 200 test failed
+})
 
 //Lesson 9 same testcases in simple-solution-api.spec.ts
 test('123 post order with correct data should receive code 201', async ({ request }) => {
-    // prepare request body
-    const requestBody = {
-        status: 'OPEN',
-        courierId: 0,
-        customerName: 'string',
-        customerPhone: 'string',
-        comment: 'string',
-        id: 0,
-    };
+  // prepare request body
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 0,
+  }
 
-    // Send a POST request to the server
-    const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-        data: requestBody,
-    });
-    const responseBody = await response.json();
+  // Send a POST request to the server
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: requestBody,
+  })
+  const responseBody = await response.json()
 
-    // Log the response status and body
-    console.log('response status:', response.status());
-    console.log('response body:', responseBody);
-    expect(response.status()).toBe(StatusCodes.OK);
-    expect(responseBody.status).toBe('OPEN');
-});
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', responseBody)
+  expect(response.status()).toBe(StatusCodes.OK)
+  expect(responseBody.status).toBe('OPEN')
+})
 //Lesson 9
 test('post order with data without status field should receive code 200', async ({ request }) => {
-    // prepare request body
-    const requestBody = {
-        courierId: 0,
-        customerName: 'string',
-        customerPhone: 'string',
-        comment: 'string',
-        id: 0,
-    };
-    // Send a POST request to the server
-    const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-        data: requestBody,
-    });
-    const responseBody = await response.json();
+  // prepare request body
+  const requestBody = {
+    courierId: 0,
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 0,
+  }
+  // Send a POST request to the server
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: requestBody,
+  })
+  const responseBody = await response.json()
 
-    // Log the response status and body
-    console.log('response status:', response.status());
-    console.log('response body:', responseBody);
-    expect(response.status()).toBe(StatusCodes.OK);
-    expect(responseBody.status).toBeNull();
-});
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', responseBody)
+  expect(response.status()).toBe(StatusCodes.OK)
+  expect(responseBody.status).toBeNull()
+})
 
 //Lesson 9
 test('post order with empty data should receive code 200', async ({ request }) => {
-    // prepare request body
-    const requestBody = {}
-    // Send a POST request to the server
-    const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-        data: requestBody,
-    });
-    const responseBody = await response.json();
+  // prepare request body
+  const requestBody = {}
+  // Send a POST request to the server
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: requestBody,
+  })
+  const responseBody = await response.json()
 
-    // Log the response status and body
-    console.log('response status:', response.status());
-    console.log('response body:', responseBody);
-    expect(response.status()).toBe(StatusCodes.OK);
-    expect(responseBody.status).toBeNull();
-});
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', responseBody)
+  expect(response.status()).toBe(StatusCodes.OK)
+  expect(responseBody.status).toBeNull()
+})
--- tests/delOrder.spec.ts
+++ tests/delOrder.spec.ts
@@ -1,52 +1,53 @@
 import { expect, test } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
 
-    test('Delete order with valid ID and get 204 OK', async ({ request }) => {
-        const requestHeaders = {
-            api_key: "1234567895678998",
-        };
-        const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/1', {
-            headers: requestHeaders
-        });
-        console.log('Response status:', response.status());
-        expect(response.status()).toBe(204);
-    });
+test('Delete order with valid ID and get 204 OK', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567895678998',
+  }
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/1', {
+    headers: requestHeaders,
+  })
+  console.log('Response status:', response.status())
+  expect(response.status()).toBe(204)
+})
 
-    test('Delete order with empty ID and get 405 error', async ({ request }) => {
-        const requestHeaders = {
-            api_key: "1234567895678998",
-        };
-        const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/', {
-            headers: requestHeaders
-        });
-        console.log('Response status:', response.status());
-        expect(response.status()).toBe(405); //expected status 405
-    });
+test('Delete order with empty ID and get 405 error', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567895678998',
+  }
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/', {
+    headers: requestHeaders,
+  })
+  console.log('Response status:', response.status())
+  expect(response.status()).toBe(405) //expected status 405
+})
 
-    test('Delete order with invalid token and get 401 error', async ({ request }) => {
-        const response = await request.delete('http://backend.tallinn-learning.ee/test-orders/1', {
-            headers: {
-                api_key: "128",
-            },
-        });
-        console.log('Response status:', response.status());
-        expect(response.status()).toBe(StatusCodes.UNAUTHORIZED); //Expected status 401
-    });
+test('Delete order with invalid token and get 401 error', async ({ request }) => {
+  const response = await request.delete('http://backend.tallinn-learning.ee/test-orders/1', {
+    headers: {
+      api_key: '128',
+    },
+  })
+  console.log('Response status:', response.status())
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED) //Expected status 401
+})
 
-    test('Delete order with invalid ID format and get 400 error', async ({ request }) => {
-        const response = await request.delete('http://backend.tallinn-learning.ee/test-orders/invalid-id', {
-        });
-        console.log('Response status:', response.status());
-        expect(response.status()).toBe(StatusCodes.BAD_REQUEST); //Expected status 400
-    });
+test('Delete order with invalid ID format and get 400 error', async ({ request }) => {
+  const response = await request.delete(
+    'http://backend.tallinn-learning.ee/test-orders/invalid-id',
+    {},
+  )
+  console.log('Response status:', response.status())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST) //Expected status 400
+})
 
-    test('Delete order with empty api_key and get 401 error', async ({ request }) => {
-        const response = await request.delete('http://backend.tallinn-learning.ee/test-orders/1', {
-            headers: {
-                api_key: "",
-            },
-        });
-        console.log('Response status:', response.status());
-        expect(response.status()).toBe(StatusCodes.UNAUTHORIZED); //Expected status 401
-    });
-
+test('Delete order with empty api_key and get 401 error', async ({ request }) => {
+  const response = await request.delete('http://backend.tallinn-learning.ee/test-orders/1', {
+    headers: {
+      api_key: '',
+    },
+  })
+  console.log('Response status:', response.status())
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED) //Expected status 401
+})
