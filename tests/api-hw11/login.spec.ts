2f54371f3279c13e03425ad4209b5f6992feb173
Mpakovich <56260995f@gmail.com>
2025-02-01 19:21:12 +0200

    ulutshil chitaemost'

--- tests/api-hw11/login.spec.ts
+++ tests/api-hw11/login.spec.ts
@@ -1,25 +1,25 @@
-import { expect, test } from '@playwright/test'
-import { LoginDTO } from '../dto/LoginDTO'
-import { StatusCodes } from 'http-status-codes'
+import { expect, test } from '@playwright/test';
+import { LoginDTO } from '../dto/LoginDTO';
+import { StatusCodes } from 'http-status-codes';
 
-test.describe('Login tests', async () => {
+test.describe('Login tests', () => {
   test('Successful authorization', async ({ request }) => {
     const response = await request.post(`https://backend.tallinn-learning.ee/login/student`, {
       data: LoginDTO.createLoginWithCorrectData(),
-    })
+    });
 
-    console.log(await response.text())
-    expect(response.status()).toBe(StatusCodes.OK)
+    console.log('Response body:', await response.text());
+    expect(response.status()).toBe(StatusCodes.OK);
 
-    expect(await response.text()).toMatch(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/)
-  })
+    expect(await response.text()).toMatch(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/);
+  });
 
   test('Unsuccessful authorization with wrong HTTP method', async ({ request }) => {
     const response = await request.get(`https://backend.tallinn-learning.ee/login/student`, {
       data: LoginDTO.createLoginWithCorrectData(),
-    })
+    });
 
-    console.log(await response.json())
-    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
-  })
-})
+    console.log('Response body:', await response.json());
+    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED);
+  });
+});
