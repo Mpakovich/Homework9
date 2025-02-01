c88472075d7d6287722f01fa011eb15099ab8f2c
Mpakovich <56260995f@gmail.com>
2025-02-01 17:22:21 +0200

    prettier fix

--- tests/api-hw11/login.spec.ts
+++ tests/api-hw11/login.spec.ts
@@ -1,26 +1,26 @@
 import { expect, test } from '@playwright/test'
-import { LoginDTO} from "../dto/LoginDTO";
+import { LoginDTO } from '../dto/LoginDTO'
 import { StatusCodes } from 'http-status-codes'
 
 test.describe('Login tests', async () => {
-    test('Successful authorization', async ({ request }) => {
-        const response = await request.post(`https://backend.tallinn-learning.ee/login/student`, {
-            data: LoginDTO.createLoginWithCorrectData(),
-        });
+  test('Successful authorization', async ({ request }) => {
+    const response = await request.post(`https://backend.tallinn-learning.ee/login/student`, {
+      data: LoginDTO.createLoginWithCorrectData(),
+    })
 
-        console.log(await response.text());
-        expect(response.status()).toBe(StatusCodes.OK);
+    console.log(await response.text())
+    expect(response.status()).toBe(StatusCodes.OK)
 
-        // Проверка JWT с использованием expect
-        expect(await response.text()).toMatch(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/);
-    });
+    // Проверка JWT с использованием expect
+    expect(await response.text()).toMatch(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/)
+  })
 
-    test('Unsuccessful authorization with wrong HTTP method', async ({ request }) => {
-        const response = await request.get(`https://backend.tallinn-learning.ee/login/student`, {
-            data: LoginDTO.createLoginWithCorrectData(),
-        });
+  test('Unsuccessful authorization with wrong HTTP method', async ({ request }) => {
+    const response = await request.get(`https://backend.tallinn-learning.ee/login/student`, {
+      data: LoginDTO.createLoginWithCorrectData(),
+    })
 
-        console.log(await response.json());
-        expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED);
-    });
-});
+    console.log(await response.json())
+    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+  })
+})
