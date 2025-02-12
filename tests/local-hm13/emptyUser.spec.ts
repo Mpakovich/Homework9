72550ade5e7e91bad26ce490aa653603d536245e
Mpakovich <56260995f@gmail.com>
2025-02-12 17:55:50 +0200

    fix

--- tests/local-hm13/emptyUser.spec.ts
+++ tests/local-hm13/emptyUser.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 import { UserDTO } from '../dto/UserDTO'
 
-test('TL-14-4 del all users test', async ({ request }) => {
+test('TL-14-5 del all users test', async ({ request }) => {
   const allUsersBefore = await request.get('http://localhost:3000/users')
   const usersBefore: UserDTO[] = await allUsersBefore.json()
 
@@ -11,11 +11,5 @@ test('TL-14-4 del all users test', async ({ request }) => {
   const allUsersResponse = await request.get('http://localhost:3000/users')
   const json: UserDTO[] = await allUsersResponse.json()
 
-  expect(json.length).toBe(0)
-})
-
-test('TL-14-5 get empty array all users test', async ({ request }) => {
-  const allUsersResponse = await request.get('http://localhost:3000/users')
-  const json = await allUsersResponse.json()
   expect(json.length).toBe(0)
 })
