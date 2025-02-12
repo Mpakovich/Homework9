bb299a6f2280f03a1c97a1b91ee422659b7dbf0b
Mpakovich <56260995f@gmail.com>
2025-02-12 14:21:04 +0200

    fix

--- tests/dto/UserDTO.ts
+++ tests/dto/UserDTO.ts
@@ -1,17 +1,17 @@
 export class UserDTO {
-    id: number
-    name: string
-    email: string
-    phone: string
+  id: number
+  name: string
+  email: string
+  phone: string
 
-    constructor(id: number, name: string, email: string, phone: string) {
-        this.id = id
-        this.name = name
-        this.email = email
-        this.phone = phone
-    }
+  constructor(id: number, name: string, email: string, phone: string) {
+    this.id = id
+    this.name = name
+    this.email = email
+    this.phone = phone
+  }
 
-    static serializeResponse(json: any): UserDTO {
-        return new UserDTO(json?.id, json?.name, json?.email, json?.phone)
-    }
+  static serializeResponse(json: any): UserDTO {
+    return new UserDTO(json?.id, json?.name, json?.email, json?.phone)
+  }
 }
